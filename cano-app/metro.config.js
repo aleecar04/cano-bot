d38e8e6a6656ff7f4509945eae63734244fa0dd1
Alejandro Carmona Reina <alecarrei1@alum.us.es>
2026-03-02 10:14:21 +0100

    chore: Archivos de configuración para usar tailwind en frontend

---
+++ cano-app/metro.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(path.resolve(__dirname));
+
+module.exports = withNativeWind(config, { input: './global.css' });
