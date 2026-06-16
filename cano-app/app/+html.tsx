0c77f935651cbd5dbf9b4c1497cc59097842305b
Alejandro Carmona Reina <alecarrei1@alum.us.es>
2026-06-16 23:39:46 +0200

    fix: render en iphone

--- cano-app/app/+html.tsx
+++ cano-app/app/+html.tsx
@@ -7,7 +7,7 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, interactive-widget=overlays-content" />
         <title>Cano-Bot</title>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
